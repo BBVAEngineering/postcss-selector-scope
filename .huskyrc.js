a5b33c7afaa4a1e546295abe00bdd7a3bb4c1313
Kus Cámara <kcmr@users.noreply.github.com>
2019-04-24 22:56:43 +0200

    chore(husky): move hooks to package

--- .huskyrc.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-	hooks: {
-		'pre-commit': 'lint-staged',
-		'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-		'pre-push': 'npm run lint:js && npm test'
-	}
-};
