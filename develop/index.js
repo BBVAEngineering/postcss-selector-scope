011a1c0518022bc3a1e109a55a1d4db6b1ea0e2a
Kus Cámara <kcmr@users.noreply.github.com>
2019-04-24 22:21:40 +0200

    test(develop): include failing cases

--- develop/index.js
+++ develop/index.js
@@ -1,16 +1,18 @@
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
 const postcss = require('postcss');
 const plugin = require('../lib');
 
+const fixture = 'develop';
 const fixturePath = path.join(process.cwd(), 'tests', 'fixtures');
-const read = (file) => fs.readFileSync(path.join(fixturePath, file), 'utf-8');
-const input = read('develop.css');
+const input = fs.readFileSync(path.join(fixturePath, `${fixture}.css`), 'utf-8');
+
+const write = (result) => fs.writeFileSync(path.join(fixturePath, `${fixture}-processed.css`), result.css, 'utf-8');
 
 postcss()
 	.use(plugin)
-	.process(input, { from: undefined, to: undefined })
-	.then((result) => {
-		console.log(result.css);
-	})
+	.process(input, { from: undefined })
+	.then(write)
 	.catch(console.error);
