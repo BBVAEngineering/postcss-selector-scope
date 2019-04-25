863981643b915710dff886403dc70a63924a37ef
Kus Cámara <kcmr@users.noreply.github.com>
2019-04-25 23:53:36 +0200

    fix: escape regexp identifiers in string options

--- develop/index.js
+++ develop/index.js
@@ -6,17 +6,28 @@ const postcss = require('postcss');
 const plugin = require('../lib');
 
 const fixture = 'develop';
-const fixturePath = path.join(process.cwd(), 'tests', 'fixtures');
-const input = fs.readFileSync(path.join(fixturePath, `${fixture}.css`), 'utf-8');
+const inputPath = path.join(process.cwd(), 'tests/fixtures/develop');
+const outputPath = path.join(process.cwd(), 'tests/fixtures/develop-output');
+const input = fs.readFileSync(path.join(inputPath, `${fixture}.css`), 'utf-8');
 
 const write = (result) => {
 	console.log(`\n⚡️ ${fixture}-processed.css updated\n`);
 
-	fs.writeFileSync(path.join(fixturePath, `${fixture}-processed.css`), result.css);
+	fs.writeFileSync(path.join(outputPath, `${fixture}-processed.css`), result.css);
 };
 
 postcss()
-	.use(plugin)
+	.use(
+		plugin({
+			exclude: [
+				'.excluded',
+				'[data-icon]',
+				':not(.something)',
+				'#id',
+				'strong'
+			]
+		})
+	)
 	.process(input, { from: undefined })
 	.then(write)
 	.catch(console.error);
